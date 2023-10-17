5667e954c9e63276fae8ed856871782f46ad518d
Abdelaali Ezmamti <116950215+AbdoxEz@users.noreply.github.com>
2023-10-17 10:07:13 +0000

    Add files via upload

---
+++ master.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+  
+
+const sideMenu = document.querySelector("#services");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click' , () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click' , () => {
+    sideMenu.style.display = 'none';
+})
+
